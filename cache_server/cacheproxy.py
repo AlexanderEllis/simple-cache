32853f1eca25d5359c3a628531099220331f15c0
Alexander Ellis <ellisonjtk@gmail.com>
2018-05-26 09:13:29 -0400

    Refactor cache code to be much easier to present

--- cache_server/cacheproxy.py
+++ cache_server/cacheproxy.py
@@ -4,90 +4,111 @@
 """
 
 import socket
-
 from urllib.request import Request, urlopen, HTTPError
-
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('port')
-args = parser.parse_args()
 
-# Define socket host and port
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port')
+    args = parser.parse_args()
 
-SERVER_HOST = '0.0.0.0'
-SERVER_PORT = int(args.port)
+    # Define socket host and port
 
-# Initialize socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((SERVER_HOST, SERVER_PORT))
+    SERVER_HOST = '0.0.0.0'
+    SERVER_PORT = int(args.port)
 
-server_socket.listen(1)
+    # Initialize socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((SERVER_HOST, SERVER_PORT))
 
-print('Cache proxy is listening on port %s ...' % SERVER_PORT)
+    server_socket.listen(1)
 
-while True:
-    # Wait for client connection
-    client_connection, client_address = server_socket.accept()
+    print('Cache proxy is listening on port %s ...' % SERVER_PORT)
 
-    # Get the client request
-    request = client_connection.recv(1024).decode()
-    print(request)
+    while True:
+        # Wait for client connection
+        client_connection, client_address = server_socket.accept()
 
-    # Parse HTTP headers
-    headers = request.split('\n')
+        # Get the client request
+        request = client_connection.recv(1024).decode()
+        print(request)
 
-    top_header = headers[0].split()
-    method = top_header[0]
-    filename = top_header[1]
+        # Parse HTTP headers
+        headers = request.split('\n')
 
-    # Index check
-    if filename == '/':
-        filename = '/index.html'
+        top_header = headers[0].split()
+        method = top_header[0]
+        filename = top_header[1]
 
-    # Let's try to read the file locally
+        # Index check
+        if filename == '/':
+            filename = '/index.html'
 
+        content = fetch_file(filename)
+
+        if content:
+            response = 'HTTP/1.0 200 OK\n\n' + content
+        else:
+            response = 'HTTP/1.0 404 NOT FOUND\n\n File Not Found'
+
+        client_connection.sendall(response.encode())
+        client_connection.close()
+
+    # Close socket
+    server_socket.close()
+
+
+def fetch_file(filename):
+    # Let's try to read the file locally first
+    file_from_cache = fetch_from_cache(filename)
+
+    if file_from_cache:
+        print('Fetched successfully from cache.')
+        return file_from_cache
+    else:
+        print('Not in cache. Fetching from server.')
+        file_from_server = fetch_from_server(filename)
+
+        if file_from_server:
+            save_in_cache(filename, file_from_server)
+            return file_from_server
+        else:
+            return None
+
+
+def fetch_from_cache(filename):
     try:
         # Check if we have this file locally
         fin = open('cache' + filename)
         content = fin.read()
         fin.close()
-
         # If we have it, let's send it
-        print('Got from cache.')
-        response = 'HTTP/1.0 200 OK\n\n' + content
-
+        return content
     except IOError:
+        return None
 
-        # Don't have it. Have to fetch from cache
-        print('Not in cache. Fetching from server.')
 
-        # Create url
-        url = 'http://127.0.0.1:8000' + filename
-        q = Request(url)
+def fetch_from_server(filename):
+    url = 'http://127.0.0.1:8000' + filename
+    q = Request(url)
 
-        try:
-            # Send req to server and get response
-            response = urlopen(q)
+    try:
+        response = urlopen(q)
+        # Grab the header and content from the server req
+        response_headers = response.info()
+        content = response.read().decode('utf-8')
+        return content
+    except HTTPError:
+        return None
 
-            # Grab the header and content from the server req
-            response_headers = response.info()
-            content = response.read().decode('utf-8')
 
-            cached_file = open('cache' + filename, 'w')
-            cached_file.write(content)
-            cached_file.close()
+def save_in_cache(filename, content):
+    print('Saving a copy of {} in the cache'.format(filename))
+    cached_file = open('cache' + filename, 'w')
+    cached_file.write(content)
+    cached_file.close()
 
-            response = 'HTTP/1.0 200 OK\n\n' + content
-
-        except HTTPError:
-
-            response = 'HTTP/1.0 404 NOT FOUND\n\n File Not Found'
-
-    client_connection.sendall(response.encode())
-    client_connection.close()
-
-# Close socket
-server_socket.close()
-
+if __name__ == '__main__':
+    main()
